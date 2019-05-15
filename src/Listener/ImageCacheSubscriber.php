d72f9fbea03f8514e61e96f2c3d2624734f529d8
Raphaël Frantzen <frant.raphael@gmail.com>
2019-05-15 09:44:51 +0200

    Sauvegarde150519I

---
+++ src/Listener/ImageCacheSubscriber.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Listener;
+
+use App\Entity\Evenement;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\PreFlushEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Event\LifeCycleEventArgs;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
+
+class ImageCacheSubscriber implements EventSubscriber {
+
+    /**
+     * @var CacheManager
+     */
+    private $cacheManager;
+
+    /**
+     * @var UploaderHelper
+     */
+    private $uploaderHelper;
+
+    public function __construct(CacheManager $cacheManager, UploaderHelper $uploaderHelper){
+        $this->cacheManager= $cacheManager;
+        $this->uploaderHelper = $uploaderHelper;
+    }
+
+    public function getSubscribedEvents(){
+        return [
+            'preRemove',
+            'preUpdate'
+        ];
+    }
+
+    public function preRemove(LifeCycleEventArgs $args){
+
+        $entity = $args->getEntity();
+        if(!$entity instanceof Evenement){
+            return;
+        }
+        $this->cacheManager->remove($this->uploaderHelper->asset($entity, 'imageFile'));
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args){
+
+        $entity = $args->getEntity();
+        
+        if(!$entity instanceof Evenement){
+            return;
+        }
+
+        if($entity->getImageFile() instanceof UploadedFile){
+
+            $this->cacheManager->remove($this->uploaderHelper->asset($entity, 'imageFile'));
+
+        }
+
+    }
+}
+
+?>
